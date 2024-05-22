d2aa567014bc7d95e274f3fa42efd818d37a3ffa
mynjstr <jestertiedra@gmail.com>
2024-05-22 23:42:13 +0800

    created constructor for TV class

--- class_tv.py
+++ class_tv.py
@@ -1,6 +1,11 @@
 # class tv program
 # Define a class TV with attributes and methods
 class TV:
-    
+    def __init__(self, name_of_tv, channel, level_of_volume, on):
+        self.TVName = name_of_tv
+        self.channel = channels
+        self.VolumeLevel = level_of_volume
+        self.on = on
+        
 # Initialize the TV object with given parameters
 # Define the methods for TV object
