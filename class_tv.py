d3537959f8e1ea58c228753ef13b9690fcc17878
mynjstr <jestertiedra@gmail.com>
2024-05-23 10:13:29 +0800

    casings adjusted

--- class_tv.py
+++ class_tv.py
@@ -3,37 +3,37 @@
 class TV:
     # Initialize the TV object with given parameters
     def __init__(self, name_of_tv, channel, level_of_volume, on):
-        self.tvname = name_of_tv
+        self.tv_name = name_of_tv
         self.channel = channel
-        self.volumelevel = level_of_volume
+        self.volume_level = level_of_volume
         self.on = on
          # Validate channel and volume levels
         if self.channel < 1:
             self.channel = 1
-            print(f"{self.tvname}: Channels lower than 1 are not available")
+            print(f"{self.tv_name}: Channels lower than 1 are not available")
         
         if self.channel > 120:
             self.channel = 120
-            print(f"{self.tvname}: The channel limit is 120")
+            print(f"{self.tv_name}: The channel limit is 120")
 
         if self.volumelevel > 7:
             self.channel = 7
-            print(f"{self.tvname}: The highest volume is 7")
+            print(f"{self.tv_name}: The highest volume is 7")
         
         if self.volumelevel < 1:
             self.volumelevel = 1
-            print(f"{self.tvname}: The lowest volume is 1")
-            
+            print(f"{self.tv_name}: The lowest volume is 1")
+
 # Define the methods for TV object
     #Turn off the TV    
-    def turnoff(self):
+    def turn_off(self):
         self.on = False
-        print(f"{self.tvname} is turned off")
+        print(f"{self.tv_name} is turned off")
 
     #Turn on the TV
-    def turnon(self):
+    def turn_on(self):
         self.on = True
-        print(f"{self.tvname} is turned on")
+        print(f"{self.tv_name} is turned on")
 
     
     
