099ff3f5788c884ff09a64614bccc0288cc9f2a2
mynjstr <jestertiedra@gmail.com>
2024-05-23 12:47:37 +0800

    created tv1 and tv2

--- test_tv.py
+++ test_tv.py
@@ -1,6 +1,9 @@
 #call class tv
 #Import TV from class_tv.py
+from class_tv import TV
 #Create a TV object named tv1 and tv2 with parameters
+tv1 = TV('tv1', 69, 10, True)
+tv2 = TV('tv2', 6, 7, False)
 #tv1
 #tv2
 #Show the result
